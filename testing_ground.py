4bd059ad40f77108ddc04cb6ac2a813db6632011
RatStanley <StanleyRat@o2.pl>
2022-01-21 15:42:55 +0100

    testing

--- testing_ground.py
+++ testing_ground.py
@@ -1,7 +1,7 @@
 import cv2
 import json
 import click
-
+import copy
 from glob import glob
 
 import numpy as np
@@ -14,66 +14,101 @@ def empty_callback(value):
     pass
 
 
-def detect_fruits(img_path: str) -> Dict[str, int]:
+img_org = cv2.imread("data/00.jpg", cv2.IMREAD_COLOR)
+scale = 0.2
+size_of_view = (int(img_org.shape[1] * scale), int(img_org.shape[0] * scale))
+img_org = cv2.resize(img_org, dsize=size_of_view)
 
-    cv2.namedWindow('result')
-    cv2.createTrackbar('H', 'result', 0, 255, empty_callback)
-    cv2.createTrackbar('S', 'result', 0, 255, empty_callback)
-    cv2.createTrackbar('V', 'result', 0, 255, empty_callback)
-    cv2.createTrackbar('R', 'result', 0, 255, empty_callback)
-    cv2.createTrackbar('G', 'result', 0, 255, empty_callback)
-    cv2.createTrackbar('B', 'result', 0, 255, empty_callback)
+img_show = img_org[:]
+img_rgb = img_org[:]  # np.zeros(img_show.shape)
 
-    img_org = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    scale = 0.1
-    size_of_view = (int(img_org.shape[1] * scale), int(img_org.shape[0] * scale))
-    img_org = cv2.resize(img_org, dsize=size_of_view)
-    img_show = img_org
-    img_rgb = img_org
-    img_hsv = cv2.cvtColor(img_org, cv2.COLOR_BGR2HSV)
-    img_hsv_converted = img_hsv
-    img = np.concatenate((img_show, img_hsv, img_rgb), axis=1)
-    key = ord('a')
-    print(img.shape)
-    while key != ord('q'):
+img_hsv = cv2.cvtColor(img_org, cv2.COLOR_BGR2HSV)
 
-        # Wait a little (30 ms) for a key press - this is required to refresh the image in our window
-        H = cv2.getTrackbarPos('H', 'result')
-        S = cv2.getTrackbarPos('S', 'result')
-        V = cv2.getTrackbarPos('V', 'result')
-        R = cv2.getTrackbarPos('R', 'result')
-        G = cv2.getTrackbarPos('G', 'result')
-        B = cv2.getTrackbarPos('B', 'result')
-        # H_value = img_hsv[:,:,0]
-        # S_value = img_hsv[:,:,1]
-        # V_value = img_hsv[:,:,2]
-        # print(H_value.shape)
+img_show = copy.deepcopy(img_org)
+img_hsv_converted = copy.deepcopy(img_hsv)
 
-        ret, H_value = cv2.threshold(img_hsv[:,:,0], H, 255, cv2.THRESH_TOZERO)
-        ret, S_value = cv2.threshold(img_hsv[:,:,1], S, 255, cv2.THRESH_TOZERO)
-        ret, V_value = cv2.threshold(img_hsv[:,:,2], V, 255, cv2.THRESH_TOZERO)
-        ret, R_value = cv2.threshold(img_org[:,:,2], R, 255, cv2.THRESH_TOZERO)
-        ret, G_value = cv2.threshold(img_org[:,:,1], G, 255, cv2.THRESH_TOZERO)
-        ret, B_value = cv2.threshold(img_org[:,:,0], B, 255, cv2.THRESH_TOZERO)
-        img_hsv_converted[:,:,0] = H_value
-        img_hsv_converted[:,:,1] = S_value
-        img_hsv_converted[:,:,2] = V_value
+img_rgb = img_show  # np.zeros(img_show.shape)
+# img = np.concatenate((img_show, img_hsv, img_rgb), axis=1)
+img = np.hstack((img_show, img_hsv))
+img = np.hstack((img, img_rgb))
 
-        img_rgb[:,:,2] = R_value
-        img_rgb[:,:,1] = G_value
-        img_rgb[:,:,0] = B_value
+H_Orgin = img_hsv[:, :, 0]
+S_Orgin = img_hsv[:, :, 1]
+V_Orgin = img_hsv[:, :, 2]
 
-        img = np.concatenate((img_show, img_hsv_converted, img_rgb), axis=1)
-        cv2.imshow('result', img)
-        key = cv2.waitKey(30)
-    # TODO: Implement detection method.
+gray = cv2.cvtColor(img_org, cv2.COLOR_RGB2GRAY)
 
-    apple = 0
-    banana = 0
-    orange = 0
-    print("test")
+cv2.namedWindow('result')
+cv2.namedWindow('img_show')
 
-    return {'apple': apple, 'banana': banana, 'orange': orange}
+cv2.resizeWindow('result', 900, 900)
 
+cv2.createTrackbar('HL', 'result', 0, 255, empty_callback)
 
-detect_fruits("data/00.jpg")
+cv2.createTrackbar('SL', 'result', 0, 255, empty_callback)
+
+cv2.createTrackbar('VL', 'result', 0, 255, empty_callback)
+
+cv2.createTrackbar('RL', 'result', 0, 255, empty_callback)
+
+cv2.createTrackbar('GL', 'result', 0, 255, empty_callback)
+
+cv2.createTrackbar('BL', 'result', 0, 255, empty_callback)
+
+key = ord('a')
+
+ret, H_value = cv2.threshold(H_Orgin, 167, 255, cv2.THRESH_BINARY)
+ret, S_value = cv2.threshold(S_Orgin, 104, 255, cv2.THRESH_BINARY)
+ret, V_value = cv2.threshold(V_Orgin, 235, 255, cv2.THRESH_BINARY)
+
+img_hsv_converted[:, :, 0] = H_value
+img_hsv_converted[:, :, 1] = S_value
+img_hsv_converted[:, :, 2] = V_value
+# print(img_hsv_converted.shape)
+
+rgb_mod = cv2.cvtColor(img_hsv_converted, cv2.COLOR_HSV2BGR)
+# x = 0
+# print(rgb_mod.shape)
+#
+# while x != 1:
+#     cv2.imshow('img_show',rgb_mod)
+#     key = cv2.waitKey(30)
+while key != ord('q'):
+    # Wait a little (30 ms) for a key press - this is required to refresh the image in our window
+    HL = cv2.getTrackbarPos('HL', 'result')
+    SL = cv2.getTrackbarPos('SL', 'result')
+    VL = cv2.getTrackbarPos('VL', 'result')
+    RL = cv2.getTrackbarPos('RL', 'result')
+    GL = cv2.getTrackbarPos('GL', 'result')
+    BL = cv2.getTrackbarPos('BL', 'result')
+
+    ret, H_value = cv2.threshold(H_Orgin, HL, 255, cv2.THRESH_BINARY)
+    ret, S_value = cv2.threshold(S_Orgin, SL, 255, cv2.THRESH_BINARY)
+    ret, V_value = cv2.threshold(V_Orgin, VL, 255, cv2.THRESH_BINARY)
+    ret, R_value = cv2.threshold(rgb_mod[:, :, 2], RL, 255, cv2.THRESH_TOZERO)
+    ret, G_value = cv2.threshold(rgb_mod[:, :, 1], GL, 255, cv2.THRESH_TOZERO)
+    ret, B_value = cv2.threshold(rgb_mod[:, :, 0], BL, 255, cv2.THRESH_TOZERO)
+
+    img_hsv_converted[:, :, 0] = H_value
+    img_hsv_converted[:, :, 1] = S_value
+    img_hsv_converted[:, :, 2] = V_value
+
+    gray = cv2.bitwise_and(img_hsv_converted, img_hsv_converted, img_hsv_converted)
+    gray = cv2.cvtColor(gray, cv2.COLOR_RGB2GRAY)
+    
+    img_rgb[:, :, 2] = R_value
+    img_rgb[:, :, 1] = G_value
+    img_rgb[:, :, 0] = B_value
+
+    img = np.hstack((img_org, img_hsv_converted))
+    img = np.hstack((img, img_rgb))
+    cv2.imshow('result', img)
+    cv2.imshow('img_show', gray)
+
+    key = cv2.waitKey(30)
+# TODO: Implement detection method.
+
+apple = 0
+banana = 0
+orange = 0
+print("test")
