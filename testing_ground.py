b283f22f874abf57a5f5017c9059519362d9464f
RatStanley <StanleyRat@o2.pl>
2022-02-01 14:40:07 +0100

    add function looking for contures

--- testing_ground.py
+++ testing_ground.py
@@ -11,72 +11,80 @@ cv2.namedWindow('result')
 key = ord('a')
 #######################################################################################################################
 
-def image_to_contur(img):
-    banana = cv2.imread("masks/BANAN.JPG")
-    banana_mask = banana.copy()
-    banana_mask = cv2.cvtColor(banana_mask, cv2.COLOR_BGR2HSV)
+def image_to_contur(path):
+    img = cv2.imread("masks/BANAN.JPG")
+    img = img.copy()
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    H_Orgin = banana_mask[:, :, 0]
-    S_Orgin = banana_mask[:, :, 1]
-    V_Orgin = banana_mask[:, :, 2]
+    H_Orgin = img[:, :, 0]
+    S_Orgin = img[:, :, 1]
+    V_Orgin = img[:, :, 2]
     ret, H_value = cv2.threshold(H_Orgin, 220, 255, cv2.THRESH_BINARY)
     ret, S_value = cv2.threshold(S_Orgin, 100, 255, cv2.THRESH_BINARY)  # 104
     ret, V_value = cv2.threshold(V_Orgin, 202, 255, cv2.THRESH_BINARY)
-    banana_mask[:, :, 0] = H_value
-    banana_mask[:, :, 1] = S_value
-    banana_mask[:, :, 2] = V_value
+    img[:, :, 0] = H_value
+    img[:, :, 1] = S_value
+    img[:, :, 2] = V_value
 
-    banana_mask = cv2.medianBlur(banana_mask, 31)
+    img = cv2.medianBlur(img, 31)
 
-    banana_mask = cv2.cvtColor(banana_mask, cv2.COLOR_RGB2GRAY)
-    banana_mask[banana_mask != 0] = 255
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img[img != 0] = 255
 
-    contur_list, heirachy = cv2.findContours(banana_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contur_list, heirachy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     print(len(contur_list))
+    return contur_list,heirachy
 
-banana = cv2.imread("masks/BANAN.JPG")
-banana_mask = banana.copy()
-banana_mask = cv2.cvtColor(banana_mask, cv2.COLOR_BGR2HSV)
 
-H_Orgin = banana_mask[:, :, 0]
-S_Orgin = banana_mask[:, :, 1]
-V_Orgin = banana_mask[:, :, 2]
-ret, H_value = cv2.threshold(H_Orgin, 220, 255, cv2.THRESH_BINARY)
-ret, S_value = cv2.threshold(S_Orgin, 100, 255, cv2.THRESH_BINARY)  # 104
-ret, V_value = cv2.threshold(V_Orgin, 202, 255, cv2.THRESH_BINARY)
-banana_mask[:, :, 0] = H_value
-banana_mask[:, :, 1] = S_value
-banana_mask[:, :, 2] = V_value
 
-banana_mask = cv2.medianBlur(banana_mask, 31)
+# banana = cv2.imread("masks/BANAN.JPG")
+# banana_mask = banana.copy()
+# banana_mask = cv2.cvtColor(banana_mask, cv2.COLOR_BGR2HSV)
+#
+# H_Orgin = banana_mask[:, :, 0]
+# S_Orgin = banana_mask[:, :, 1]
+# V_Orgin = banana_mask[:, :, 2]
+# ret, H_value = cv2.threshold(H_Orgin, 220, 255, cv2.THRESH_BINARY)
+# ret, S_value = cv2.threshold(S_Orgin, 100, 255, cv2.THRESH_BINARY)  # 104
+# ret, V_value = cv2.threshold(V_Orgin, 202, 255, cv2.THRESH_BINARY)
+# banana_mask[:, :, 0] = H_value
+# banana_mask[:, :, 1] = S_value
+# banana_mask[:, :, 2] = V_value
+#
+# banana_mask = cv2.medianBlur(banana_mask, 31)
+#
+# banana_mask = cv2.cvtColor(banana_mask,cv2.COLOR_RGB2GRAY)
+# banana_mask[banana_mask != 0] = 255
+#
+# contur_list, heirachy = cv2.findContours(banana_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-banana_mask = cv2.cvtColor(banana_mask,cv2.COLOR_RGB2GRAY)
-banana_mask[banana_mask != 0] = 255
+#img_org = cv2.cvtColor(img_org, cv2.COLOR_BGR2HSV)
 
-contur_list, heirachy = cv2.findContours(banana_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# H_Orgin = img_org[:, :, 0]
+# S_Orgin = img_org[:, :, 1]
+# V_Orgin = img_org[:, :, 2]
+# ret, H_value = cv2.threshold(H_Orgin, 220, 255, cv2.THRESH_BINARY)
+# ret, S_value = cv2.threshold(S_Orgin, 100, 255, cv2.THRESH_BINARY)  # 104
+# ret, V_value = cv2.threshold(V_Orgin, 202, 255, cv2.THRESH_BINARY)
+# img_org[:, :, 0] = H_value
+# img_org[:, :, 1] = S_value
+# img_org[:, :, 2] = V_value
+# img_org = cv2.medianBlur(img_org, 31)
+#
+# img_org = cv2.cvtColor(img_org,cv2.COLOR_RGB2GRAY)
+# img_org[img_org != 0] = 255
+#
+# contur_list, heirachy = cv2.findContours(img_org, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# conturs = cv2.drawContours(img_org,contur_list,-1,(255,0,0),5)
+#
+contur_list, heirachy = image_to_contur("masks/BANAN.JPG")
 print(len(contur_list))
 #to tylko by pokazać że działa
+banana = cv2.imread("masks/BANAN.JPG")
 conturs = cv2.drawContours(banana,contur_list,-1,(255,0,0),5)
 #szukanie konturu
-img_org = cv2.cvtColor(img_org, cv2.COLOR_BGR2HSV)
 
-H_Orgin = img_org[:, :, 0]
-S_Orgin = img_org[:, :, 1]
-V_Orgin = img_org[:, :, 2]
-ret, H_value = cv2.threshold(H_Orgin, 220, 255, cv2.THRESH_BINARY)
-ret, S_value = cv2.threshold(S_Orgin, 100, 255, cv2.THRESH_BINARY)  # 104
-ret, V_value = cv2.threshold(V_Orgin, 202, 255, cv2.THRESH_BINARY)
-img_org[:, :, 0] = H_value
-img_org[:, :, 1] = S_value
-img_org[:, :, 2] = V_value
-img_org = cv2.medianBlur(img_org, 31)
-
-img_org = cv2.cvtColor(img_org,cv2.COLOR_RGB2GRAY)
-img_org[img_org != 0] = 255
-
-contur_list, heirachy = cv2.findContours(img_org, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-conturs = cv2.drawContours(img_org,contur_list,-1,(255,0,0),5)
-
+# contur_list2, heirachy2 = image_to_contur("masks/BANAN.JPG")
 
 while key != ord('q'):
     # cv2.imshow('result', gray_mask)
