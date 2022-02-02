62c87ea4d216121f40ee9cb9852782056a951a7f
RatStanley <StanleyRat@o2.pl>
2022-02-02 01:36:32 +0100

    working contures

---
+++ TRESHOLD_TESTS.py
@@ -0,0 +1,144 @@
+import cv2
+import json
+import click
+import copy
+from glob import glob
+
+import numpy as np
+from tqdm import tqdm
+
+from typing import Dict
+
+
+def empty_callback(value):
+    pass
+
+img_org = cv2.imread("masks/JABLKO_1.JPG", cv2.IMREAD_COLOR)
+scale = 0.2
+size_of_view = (int(img_org.shape[1] * scale), int(img_org.shape[0] * scale))
+img_org = cv2.resize(img_org, dsize=size_of_view)
+
+img_show = img_org[:]
+img_rgb = img_org.copy()  # np.zeros(img_show.shape)
+
+img_hsv = cv2.cvtColor(img_org, cv2.COLOR_BGR2HSV)
+
+img_show = copy.deepcopy(img_org)
+img_hsv_converted = copy.deepcopy(img_hsv)
+
+img_rgb = img_show  # np.zeros(img_show.shape)
+gray_mask = np.zeros(img_show.shape)
+# img = np.concatenate((img_show, img_hsv, img_rgb), axis=1)
+img = np.hstack((img_show, img_hsv))
+img = np.hstack((img, img_rgb))
+
+H_Orgin = img_hsv[:, :, 0]
+S_Orgin = img_hsv[:, :, 1]
+V_Orgin = img_hsv[:, :, 2]
+
+R_Orgin = img_rgb[:, :, 0]
+G_Orgin = img_rgb[:, :, 1]
+B_Orgin = img_rgb[:, :, 2]
+
+gray = cv2.cvtColor(img_org, cv2.COLOR_RGB2GRAY)
+
+# cv2.namedWindow('result')
+cv2.namedWindow('img_show')
+
+# cv2.resizeWindow('result', 900, 900)
+
+cv2.createTrackbar('HL', 'img_show', 0, 255, empty_callback)
+cv2.createTrackbar('SL', 'img_show', 0, 255, empty_callback)
+cv2.createTrackbar('VL', 'img_show', 0, 255, empty_callback)
+cv2.createTrackbar('R', 'img_show', 0, 255, empty_callback)
+cv2.createTrackbar('G', 'img_show', 0, 255, empty_callback)
+cv2.createTrackbar('B', 'img_show', 0, 255, empty_callback)
+
+key = ord('a')
+
+ret, H_value = cv2.threshold(H_Orgin, 167, 255, cv2.THRESH_BINARY)
+ret, S_value = cv2.threshold(S_Orgin, 104, 255, cv2.THRESH_BINARY)
+ret, V_value = cv2.threshold(V_Orgin, 211, 255, cv2.THRESH_BINARY)
+
+# img_hsv_converted[:, :, 0] = H_value
+# img_hsv_converted[:, :, 1] = S_value
+# img_hsv_converted[:, :, 2] = V_value
+# # print(img_hsv_converted.shape)
+#
+#
+# gray_mask = cv2.bitwise_or(gray, H_value)
+# gray_mask = cv2.bitwise_or(gray_mask, S_value)
+# gray_mask = cv2.bitwise_or(gray_mask, V_value)
+# gray_mask[gray_mask != 255] = 0
+#
+rgb_mod = np.zeros(img_show.shape)
+# rgb_mod1 = cv2.bitwise_and(img_show[:, :, 0], gray_mask)
+# rgb_mod2 = cv2.bitwise_and(img_show[:, :, 1], gray_mask)
+# rgb_mod3 = cv2.bitwise_and(img_show[:, :, 2], gray_mask)
+
+# rgb_masked = np.dstack((rgb_mod1, rgb_mod2, rgb_mod3))
+# x = 0
+# print(rgb_masked.shape)
+#
+# print(gray_mask.shape)
+#
+# while x != 1:
+#     cv2.imshow('img_show', rgb_masked)
+#     key = cv2.waitKey(30)
+
+while key != ord('q'):
+    # Wait a little (30 ms) for a key press - this is required to refresh the image in our window
+    HL = cv2.getTrackbarPos('HL', 'img_show')
+    SL = cv2.getTrackbarPos('SL', 'img_show')
+    VL = cv2.getTrackbarPos('VL', 'img_show')
+    R = cv2.getTrackbarPos('R', 'img_show')
+    G = cv2.getTrackbarPos('G', 'img_show')
+    B = cv2.getTrackbarPos('B', 'img_show')
+
+
+    ret, H_value = cv2.threshold(H_Orgin, HL, 255, cv2.THRESH_BINARY)
+    ret, S_value = cv2.threshold(S_Orgin, SL, 255, cv2.THRESH_BINARY)
+    ret, V_value = cv2.threshold(V_Orgin, VL, 255, cv2.THRESH_BINARY)
+    ret, R_value = cv2.threshold(R_Orgin, R, 255, cv2.THRESH_BINARY)
+    ret, G_value = cv2.threshold(G_Orgin, G, 255, cv2.THRESH_BINARY)
+    ret, B_value = cv2.threshold(B_Orgin, B, 255, cv2.THRESH_BINARY)
+
+
+    # img_hsv_converted[:, :, 0] = H_value
+    # img_hsv_converted[:, :, 1] = S_value
+    # img_hsv_converted[:, :, 2] = V_value
+    #
+    # img_rgb[:, :, 0] = H_value
+    # img_rgb[:, :, 1] = S_value
+    # img_rgb[:, :, 2] = V_value
+
+    gray_mask_hsv = cv2.bitwise_or(gray, H_value)
+    gray_mask_hsv = cv2.bitwise_or(gray_mask_hsv, S_value)
+    gray_mask_hsv = cv2.bitwise_or(gray_mask_hsv, V_value)
+    gray_mask_rgb = cv2.bitwise_or(gray, R_value)
+    gray_mask_rgb = cv2.bitwise_or(gray_mask_rgb, G_value)
+    gray_mask_rgb = cv2.bitwise_or(gray_mask_rgb, B_value)
+
+    gray_mask = cv2.bitwise_and(gray_mask_rgb, gray_mask_hsv)
+    # gray = cv2.cvtColor(gray, cv2.COLOR_RGB2GRAY)
+
+    # view =  cv2.cvtColor(img_hsv_converted, cv2.COLOR_RGB2GRAY)
+    # mask = cv2.bitwise_not(gray_mask)
+    gray_mask[gray_mask != 255] = 0
+    gray_mask = cv2.medianBlur(gray_mask, 3)
+
+
+    # img_rgb = cv2.bitwise_or(img_rgb, mask)
+
+
+    img = np.hstack((img, img_hsv_converted))
+    # cv2.imshow('result', img_rgb)
+    cv2.imshow('img_show', gray_mask)
+
+    key = cv2.waitKey(30)
+# TODO: Implement detection method.
+
+apple = 0
+banana = 0
+orange = 0
+print("test")
--- testing_ground.py
+++ testing_ground.py
@@ -13,7 +13,7 @@ key = ord('a')
 
 #######################################################################################################################
 
-def image_to_contur(path = " ", img_org_ = None):
+def image_to_contur(H,S,V,path = " ",img_org_ = None):
     if not path == " ":
         img = cv2.imread(path)
     else:
@@ -24,9 +24,9 @@ def image_to_contur(path = " ", img_org_ = None):
     H_Orgin = img[:, :, 0]
     S_Orgin = img[:, :, 1]
     V_Orgin = img[:, :, 2]
-    ret, H_value = cv2.threshold(H_Orgin, 200, 255, cv2.THRESH_BINARY) #220
-    ret, S_value = cv2.threshold(S_Orgin, 104, 255, cv2.THRESH_BINARY) #100 # 104
-    ret, V_value = cv2.threshold(V_Orgin, 210, 255, cv2.THRESH_BINARY) #202
+    ret, H_value = cv2.threshold(H_Orgin, H, 255, cv2.THRESH_BINARY) #220
+    ret, S_value = cv2.threshold(S_Orgin, S, 255, cv2.THRESH_BINARY) #100 # 104
+    ret, V_value = cv2.threshold(V_Orgin, V, 255, cv2.THRESH_BINARY) #202
     img[:, :, 0] = H_value
     img[:, :, 1] = S_value
     img[:, :, 2] = V_value
@@ -40,41 +40,53 @@ def image_to_contur(path = " ", img_org_ = None):
     # print(len(contur_list))
     return contur_list, heirachy
 
+def contur_compare(counturs_background_, counturs_obj,size):
+    dist_list = []
+    for cnt in counturs_background_:
+        retval = cv2.matchShapes(cnt, counturs_obj, 1, 0)
+        print(retval)
+        dist_list.append(retval)
 
-org = cv2.imread("data/09.JPG")
+    sorted_list = dist_list.copy()
+    sorted_list.sort()  # sorts the list from smallest to largest
+    obj_count = []
+    rest = []
+    for index in range(len(sorted_list)):
+        if sorted_list[index] < size:
+            ind = dist_list.index(sorted_list[index])
+            obj_count.append(counturs_background_[ind])
+        else:
+            ind = dist_list.index(sorted_list[index])
+            rest.append(counturs_background_[ind])
+    return obj_count,rest
+
+#maski obiektów
+org = cv2.imread("data/00.JPG")
 scale = 0.2
 size_of_view = (int(org.shape[1] * scale), int(org.shape[0] * scale))
 org = cv2.resize(org, dsize=size_of_view)
 
-contours_background, heirachy_bacground = image_to_contur(img_org_=org)
-ref_banana, heirachy_banana = image_to_contur("masks/BANAN.JPG")
-
-reference_contour = ref_banana[0]
-dist_list= []
-
-for cnt in contours_background:
-    retval= cv2.matchShapes(cnt, reference_contour,1,0)
-    dist_list.append(retval)
-
-sorted_list= dist_list.copy()
-sorted_list.sort() # sorts the list from smallest to largest
-banana_cnts= []
-for index in range(len(sorted_list)):
-    if sorted_list[index] < 1:
-        ind = dist_list.index(sorted_list[index])
-        banana_cnts.append(contours_background[ind])
-    else:
-        break
+contours_background, heirachy_bacground = image_to_contur(img_org_=org,H=255,S=80,V=215)#H=200,S=100,V=202
+ref_banana, heirachy_banana = image_to_contur(path="masks/BANAN.JPG",H=200,S=100,V=202)
 
 
-# ind1_dist= dist_list.index(sorted_list[0])
-# ind2_dist= dist_list.index(sorted_list[1])
-# print(len(dist_list))
+ref_orange, heirachy_orange = image_to_contur(path="masks/JABLKO_1.JPG",H=255,S=93,V=207)
 
-# banana_cnts.append(contours_background[ind1_dist])
-# banana_cnts.append(contours_background[ind2_dist])
+reference_contour_banana = ref_banana[0]
+reference_contour_orange = ref_orange[0]
+
+print(len(contours_background))
+banana_cnts,rest = contur_compare(contours_background,reference_contour_banana,1)
+print(len(rest))
+orange_cnts,rest = contur_compare(rest,reference_contour_orange,0.3)
+print(len(rest))
+
 
 with_contours = cv2.drawContours(org,banana_cnts,-1,(255,0,0),3)
+with_contours = cv2.drawContours(org,orange_cnts,-1,(0,255,0),3)
+
+
+# with_contours = cv2.drawContours(org,rest,-1,(255,255,0),3)
 
 while key != ord('q'):
     # cv2.imshow('result', gray_mask)
